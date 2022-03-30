75a2995e2a6e76c55f02f4ef2222902da1b33cf3
mariacarolinass <m.carolina.soares1@gmail.com>
2022-03-30 13:45:11 -0300

    Add métodos

--- src/main/java/mapasjava/Principal.java
+++ src/main/java/mapasjava/Principal.java
@@ -6,8 +6,8 @@ import java.util.Map;
 import static mapasjava.Municipio.pesquisarMunicipios;
 import static mapasjava.Municipio.pesquisarPopulacao;
 import static mapasjava.Municipio.pesquisarPorte;
-//import static mapasjava.Municipio.pesquisarPorteLista;
-
+import static mapasjava.Municipio.pesquisarPorteLista;
+        
 public class Principal {
 
     public static void main(String[] args) throws IOException {
@@ -16,17 +16,20 @@ public class Principal {
         
         Map <String, ArrayList<Municipio>> dados = CSV.leitura(path);
         System.out.println("Dados do Munícipio");
+        System.out.println();
         pesquisarMunicipios("RO", "Cabixi", dados);
-        System.out.println("------------------------------------------");
         
+        System.out.println("------------------------------------------");
         System.out.println("Dados da População");
+        System.out.println();
         pesquisarPopulacao("RO", dados);
         
         System.out.println("------------------------------------------");
         pesquisarPorte("RO", dados);
         
-        //System.out.println("------------------------------------------");
-        //pesquisarPorteLista("RO", dados);
+        System.out.println("------------------------------------------");
+        pesquisarPorteLista("RO", dados);
+        
     }
     
 }
